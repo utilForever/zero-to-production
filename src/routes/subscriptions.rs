94b7b732e163a590055084fc1cddd425b07b859d
Chris Ohk <utilforever@gmail.com>
2023-01-22 15:01:46 +0900

    feat: Add code to generate request ID by adding another middleware

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,7 +9,7 @@ pub struct FormData {
     name: String,
 }
 
-#[tracing::instrument(name = "Adding a new subscriber", skip(form, pool), fields(request_id = %Uuid::new_v4(), subscriber_email = %form.email, subscriber_name = %form.name))]
+#[tracing::instrument(name = "Adding a new subscriber", skip(form, pool), fields(subscriber_email = %form.email, subscriber_name = %form.name))]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     match insert_subscriber(&pool, &form).await {
         Ok(_) => HttpResponse::Ok().finish(),
