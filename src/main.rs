250b0837752febad7012f574816639f9b0ce8cb7
Chris Ohk <utilforever@gmail.com>
2023-01-22 14:13:03 +0900

    feat: Add code to print actix-web's log records

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@ use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use tracing_log::LogTracer;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    LogTracer::init().expect("Failed to set logger");
+
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
     let subscriber = Registry::default()
