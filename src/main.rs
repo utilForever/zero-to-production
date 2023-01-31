0498cbf984dfe6309f1dd07ef4da93ec1b930c49
Chris Ohk <utilforever@gmail.com>
2023-01-31 22:21:54 +0900

    feat: Replace code to call 'connect()' with 'connect_lazy()'

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,8 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
+            .expect("Failed to create Postgres connection pool.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
