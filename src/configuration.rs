7c21778237b5555a72965a865b2de7255131e541
Chris Ohk <utilforever@gmail.com>
2023-02-07 22:35:16 +0900

    refactor: Cleanup code by 'cargo clippy'

--- src/configuration.rs
+++ src/configuration.rs
@@ -58,7 +58,7 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
             configuration_directory.join("base.yaml"),
         ))
         .add_source(config::File::from(
-            configuration_directory.join(&environment_filename),
+            configuration_directory.join(environment_filename),
         ))
         .build()?;
 
