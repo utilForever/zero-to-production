dd5753d6bfcca20d86288c965762f65e266c4166
Chris Ohk <utilforever@gmail.com>
2023-02-13 21:30:22 +0900

    feat: Add code to parse 'SubscriberName' with name validation

--- src/domain.rs
+++ src/domain.rs
@@ -1,1 +1,18 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub struct SubscriberName(String);
+
+impl SubscriberName {
+    pub fn parse(s: String) -> SubscriberName {
+        let is_empty_or_whitespace = s.trim().is_empty();
+        let is_too_long = s.graphemes(true).count() > 256;
+        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+        let contains_forbidden_characters = s.chars().any(|c| forbidden_characters.contains(&c));
+
+        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
+            panic!("{s} is not a valid subscriber name.")
+        } else {
+            Self(s)
+        }
+    }
+}
