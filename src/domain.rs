d3ef08f6cd17993759aa6f243c4526c3ae0d168d
Chris Ohk <utilforever@gmail.com>
2023-02-13 21:30:40 +0900

    feat: Add struct 'NewSubscriber' for type-driven development

--- src/domain.rs
+++ src/domain.rs
@@ -1,5 +1,10 @@
 use unicode_segmentation::UnicodeSegmentation;
 
+pub struct NewSubscriber {
+    pub email: String,
+    pub name: SubscriberName,
+}
+
 pub struct SubscriberName(String);
 
 impl SubscriberName {
