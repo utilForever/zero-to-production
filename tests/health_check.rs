79009bd1acd292b1178c7b901c42995b904b58d7
Chris Ohk <utilforever@gmail.com>
2023-02-16 22:30:05 +0900

    refactor: Rename integration test to '400 Bad Request'

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -134,7 +134,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
     let test_cases = vec![
@@ -153,9 +153,9 @@ async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
             .expect("Failed to execute request.");
 
         assert_eq!(
-            200,
+            400,
             response.status().as_u16(),
-            "The API did not return a 200 OK when the payload was {description}."
+            "The API did not return a 400 Bad Request when the payload was {description}."
         );
     }
 }
