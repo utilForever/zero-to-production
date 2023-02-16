041b0d4aab5bebdfc8ba8a4b90e72c44cbb97bb2
Chris Ohk <utilforever@gmail.com>
2023-02-16 22:43:23 +0900

    refactor: Add code to handle the error returned by method 'parse()'

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,9 +12,13 @@ pub struct FormData {
 
 #[tracing::instrument(name = "Adding a new subscriber", skip(form, pool), fields(subscriber_email = %form.email, subscriber_name = %form.name))]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
     let new_subscriber = NewSubscriber {
         email: form.0.email,
-        name: SubscriberName::parse(form.0.name).expect("Name validation failed."),
+        name,
     };
 
     match insert_subscriber(&pool, &new_subscriber).await {
