d0be0249bab27cacdc7b80d128cea6f5f485b0d9
Chris Ohk <utilforever@gmail.com>
2023-02-17 20:56:33 +0900

    feat: Add some unit tests for struct 'SubscriberEmail'

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -12,3 +12,27 @@ impl AsRef<str> for SubscriberEmail {
         &self.0
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::SubscriberEmail;
+    use claims::assert_err;
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "ursuladomain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+    
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@domain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+}
