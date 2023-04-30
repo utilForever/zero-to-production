6200ebe9681a1c8ac54c4146b8f07c034aef55fa
Chris Ohk <utilforever@gmail.com>
2023-04-30 14:48:05 +0900

    feat: Add code to use the authorization token in method 'sender_email()'

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,6 +1,6 @@
 use crate::domain::SubscriberEmail;
 use reqwest::Client;
-use secrecy::Secret;
+use secrecy::{ExposeSecret, Secret};
 
 pub struct EmailClient {
     http_client: Client,
@@ -38,7 +38,14 @@ impl EmailClient {
             html_body: html_content.to_owned(),
             text_body: text_content.to_owned(),
         };
-        let builder = self.http_client.post(&url).json(&request_body);
+        let builder = self
+            .http_client
+            .post(&url)
+            .header(
+                "X-Postmark-Server-Token",
+                self.authorization_token.expose_secret(),
+            )
+            .json(&request_body);
         Ok(())
     }
 }
