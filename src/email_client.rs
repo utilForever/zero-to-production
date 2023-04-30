94703d27f8bd07157afd62bb48b783baa4759d3e
Chris Ohk <utilforever@gmail.com>
2023-04-30 14:35:04 +0900

    feat: Implement test function 'send_email_fires_a_request_to_base_url()'

--- src/email_client.rs
+++ src/email_client.rs
@@ -29,8 +29,32 @@ impl EmailClient {
 
 #[cfg(test)]
 mod tests {
+    use crate::domain::SubscriberEmail;
+    use crate::email_client::EmailClient;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::faker::lorem::en::{Paragraph, Sentence};
+    use fake::Fake;
+    use wiremock::matchers::any;
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
     #[tokio::test]
     async fn send_email_fires_a_request_to_base_url() {
-        todo!()
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender);
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
     }
 }
