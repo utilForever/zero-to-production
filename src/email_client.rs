df66ba367f5543a5f87fdad9ba49a5320abbc3e0
Chris Ohk <utilforever@gmail.com>
2023-04-30 14:06:37 +0900

    feat: Add fields 'http_client' and 'base_url' to struct 'EmailClient'

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,10 +1,21 @@
 use crate::domain::SubscriberEmail;
+use reqwest::Client;
 
 pub struct EmailClient {
+    http_client: Client,
+    base_url: String,
     sender: SubscriberEmail,
 }
 
 impl EmailClient {
+    pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
+        Self {
+            http_client: Client::new(),
+            base_url,
+            sender,
+        }
+    }
+
     pub async fn send_email(
         &self,
         recipient: SubscriberEmail,
